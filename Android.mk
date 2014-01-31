97eedeeac8d04477878e76e20285c409ddee7cac
Eric Hassold <hassold@yahoo-inc.com>
2014-01-31 11:40:31 -0800

    Add configuration to build jpegtran command line test application
    Change-Id: Ic16cdf350647d8e49846ad7c484dc2af760f61e4

--- Android.mk
+++ Android.mk
@@ -163,3 +163,13 @@ LOCAL_LDLIBS += -fuse-ld=gold
 endif
 include $(BUILD_SHARED_LIBRARY)
 endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := jpegtran
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := rdswitch.c cdjpeg.c jpegtran.c
+
+LOCAL_STATIC_LIBRARIES += libyahoo_jpegturbo
+
+include $(BUILD_EXECUTABLE)
